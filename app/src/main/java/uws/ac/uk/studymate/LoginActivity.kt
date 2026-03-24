32240eb6dddea0ddc054e3feb5b7aba25f01ba03
BecB24 <rebeccaeliz.berry@gmail.com>
2026-03-24 12:15:06 +0000

    Login + Register navigation fix, back button added

---
+++ app/src/main/java/uws/ac/uk/studymate/LoginActivity.kt
@@ -0,0 +1,21 @@
+package uws.ac.uk.studymate
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_login)
+
+        val registerText = findViewById<TextView>(R.id.registerText)
+
+        registerText.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
